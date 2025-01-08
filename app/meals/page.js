249b528dd3a018c0a349b75da9b3d377395336b4
har-sat <harsat.ponnusamy@gmail.com>
2025-01-08 23:33:05 +0530

    feat: meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,35 @@
-export default function MealsPage() {
-  return <h1>
-    Meals Page
-  </h1>
-};
+import { Suspense } from "react";
+import Link from "next/link";
+
+import classes from "./page.module.css";
+import MealsGrid from "../components/meals/meals-grid";
+import getMeals from "@/lib/meals";
+
+async function Meals() {
+  const meals = await getMeals();
+  return (
+    <Suspense fallback={<p className= {classes.loading}>Fetching Meals</p>}>
+      <MealsGrid meals={meals} />
+    </Suspense>
+  );
+}
+
+export default  function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Marvelous Meals, curated
+          <span className={classes.highlight}> for you </span>
+        </h1>
+        <p>Make your favorite recipe yourself!</p>
+        <p className={classes.cta}>
+          <Link href={"/meals/share"}>Share Your Recipies!</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Meals />
+      </main>
+    </>
+  );
+}
