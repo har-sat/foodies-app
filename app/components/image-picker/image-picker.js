9ee2d87d4f6bc06af32f92ca1171e136b23dbbdb
har-sat <harsat.ponnusamy@gmail.com>
2025-01-12 10:46:17 +0530

    feat: users can share meals

--- app/components/image-picker/image-picker.js
+++ app/components/image-picker/image-picker.js
@@ -31,26 +31,25 @@ export default function ImagePicker({ label, name }) {
     <div className={classes.picker}>
       <label htmlFor="image">{label}</label>
       <div className={classes.controls}>
-        <div className={classes.preview}>
-          {!pickedImage && <p>Pick an image</p>}
-          {pickedImage && <Image src={pickedImage} alt="user image" fill />}
-        </div>
-        <input
-          ref={imageInputRef}
-          className={classes.input}
-          type="file"
-          id={name}
-          accept="image/png image/jpeg image/hevc"
-          name={name}
-          onChange={handleImageChange}
-          required
-        />
         <button
-          className={classes.button}
+          className= {classes.button}
           type="button"
           onClick={handleSelectImageClick}
         >
-          select image
+          <div className={classes.preview}>
+            {!pickedImage && <p>Pick an image</p>}
+            {pickedImage && <Image src={pickedImage} alt="user image" fill />}
+          </div>
+          <input
+            ref={imageInputRef}
+            className={classes.input}
+            type="file"
+            id={name}
+            accept="image/png image/jpeg image/hevc"
+            name={name}
+            onChange={handleImageChange}
+            required
+          />
         </button>
       </div>
     </div>
